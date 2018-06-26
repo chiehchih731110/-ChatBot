5fff06a7b05f22caa5d39431cf2b063389f86d36
Richard <richard.lo@gudy.com.tw>
2018-06-26 23:35:16 +0800

    commit RL

--- index.js
+++ index.js
@@ -2,12 +2,11 @@
 var restify = require("restify");
 var builder = require("botbuilder");
 var request = require("request");
-var date = require("date");
 
 //Setup Web Server
 var server = restify.createServer();
 
-server.listen(process.env.port || process.env.PORT || "3978", function(){
+server.listen(process.env.port || process.env.PORT || "3978", function () {
     console.log('%s listening to %s', server.name, server.url);
 });
 
@@ -21,37 +20,64 @@ var connector = new builder.ChatConnector({
 //Listen for messages from users
 server.post('/api/messages', connector.listen());
 
-//create your bot with a function to receive messages from user
-
-var bot = new builder.UniversalBot(connector, function(session){
-    var id = session.message.text;
-    var options = {
-        method:"GET",
-        url: "https://www.alphavantage.co/query", 
-        //寫在api url ?後面的參數，要放在qs(key)的Json set內
-        qs:{
-        function: "TIME_SERIES_DAILY",
-        symbol: id,
-        apikey:"2C8MUXABNVMED4DS"
-        }, 
-        //指定json格式的輸出
-        json:true
-    }
-    request(options, function (error, response, body){
-        var stock = body;
-        //建立日期物件，放入今天的日期
-        var d = new Date();
-        //當日期是周末，則將日期回到上個周五
-        if (d.getDay()==0)
-            d.setDate(d.getDate()-1);
-        if (d.getDay()==1)
-            d.setDate(d.getDate()-2);
-        //將日期改成ISO規則日期的第0-10個字元 YYYY-mm-dd
-
-        //TODO: 更好的方式是用RegExpression, 找出JSON檔第一筆日期的資料，可以避免節慶日找不到資料
-        
-        var tradeday = d.toISOString().slice(0, 10);
-        var close = stock["Time Series (Daily)"][tradeday]["4. close"]
-        session.endDialog(`${tradeday} close at : $${close}`);
+// create your bot with a function to receive messages from user
+// 歡迎訊息頁
+var bot = new builder.UniversalBot(connector,
+    function (session) {
+        session.send('歡迎來到FinTastic');
+        session.replaceDialog('mainMenu')
     });
-});
+
+bot.dialog('mainMenu', [
+    function (session) {
+        builder.Prompts.choice(session, "請問要查詢什麼?", ["美股", "匯率", "台股", "港股", "日股"], { listStyle: builder.ListStyle.button });
+    },
+    function (session, results) {
+        session.dialogData.ask = results.response.entity;
+        if (session.dialogData.ask == "美股")
+            session.replaceDialog('us');
+        else if (session.dialogData.ask == "匯率")
+            session.replaceDialog('foreign');
+        // TODO 加入每個人寫的功能
+    }
+]);
+
+bot.dialog('us', [
+    function(session){
+        builder.Prompts.text(session, "請輸入美股Ticker:");
+    },
+    function(session, results){
+        var id = results.response;
+        var options = {
+            method: "GET",
+            url: "https://www.alphavantage.co/query",
+            //寫在api url ?後面的參數，要放在qs(key)的Json set內
+            qs: {
+                function: "TIME_SERIES_DAILY",
+                symbol: id,
+                apikey: "2C8MUXABNVMED4DS"
+            },
+            //指定json格式的輸出
+            json: true
+        }
+        request(options, function (error, response, body) {
+            var stock = body;
+            if (stock["Time Series (Daily)"]) {
+                //用RegExpression, 找出JSON檔第一筆日期的資料，可以避免節慶日找不到資料
+                var date = JSON.stringify(stock["Time Series (Daily)"]).match(/\d{4}-\d{2}-\d{2}/);
+                //parseFloat 將文字改成Float type, toFixed(2)將數字縮到小數點2位數
+                var open = parseFloat(stock["Time Series (Daily)"][date]["1. open"]).toFixed(2)
+                var high = parseFloat(stock["Time Series (Daily)"][date]["2. high"]).toFixed(2)
+                var low = parseFloat(stock["Time Series (Daily)"][date]["3. low"]).toFixed(2)
+                var close = parseFloat(stock["Time Series (Daily)"][date]["4. close"]).toFixed(2)
+                session.send(`${id.toUpperCase()} : ${date} \nopen $${open}\nhigh $${high}\nlow $${low}\nclose $${close}`);
+            } else {
+                session.send(`沒有找到這個股票!`);
+            }
+        });
+
+        // TODO 讓request資料已經完成後，才執行session.replaceDialog
+        session.replaceDialog('us');
+    }
+])
+// TODO 提供一個trigger event, 讓使用者可以回到首頁選單
