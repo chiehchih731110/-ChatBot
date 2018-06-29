7af1bcc16765256695833e7b93ce21d72c87d949
qwetom324 <39479591+qwetom324@users.noreply.github.com>
2018-06-29 17:14:41 +0800

    Update aien35.js

--- aien35.js
+++ aien35.js
@@ -1,44 +1,142 @@
-var restify = require('restify');
-var builder = require('botbuilder');
+//import module
+var restify = require("restify");
+var builder = require("botbuilder");
+var request = require("request");
+var date = require("date");
+
+//Setup Web Server
 var server = restify.createServer();
+
 server.listen(process.env.port || process.env.PORT || "3978", function () {
     console.log('%s listening to %s', server.name, server.url);
 });
 
+//create chat connector for communicating with the bot framework service
+
 var connector = new builder.ChatConnector({
-    appld: process.env.MircosoftAppld,
+    appId: process.env.MicrosoftAppId,
     appPassword: process.env.MicrosoftAppPassword,
 });
 
+//Listen for messages from users
 server.post('/api/messages', connector.listen());
 
+// create your bot with a function to receive messages from user
+// 歡迎訊息頁
 var bot = new builder.UniversalBot(connector,
     function (session) {
-        var id = session.message.text;
+        session.send('歡迎來到FinTastic');
+        session.replaceDialog('mainMenu')
+    });
+
+bot.dialog('mainMenu', [
+    function (session) {
+        builder.Prompts.choice(session, "請問要查詢什麼?", ["美股", "匯率", "台股", "港股", "日股", "黃金","歐股"], { listStyle: builder.ListStyle.button });
+    },
+    function (session, results) {
+        session.dialogData.ask = results.response.entity;
+        if (session.dialogData.ask == "美股")
+            session.replaceDialog('us');
+        else if (session.dialogData.ask == "黃金")
+            session.replaceDialog('gold');
+        else if (session.dialogData.ask == "歐股")
+            session.replaceDialog('euron');
+        // TODO 加入每個人寫的功能
+    }
+]).triggerAction({ matches: /^回首頁$/ }); //使用者任何時間打入"回首頁"都可以回到首頁
+
+bot.dialog('us', [
+    function (session) {
+        builder.Prompts.text(session, "請輸入美股Ticker:");
+
+        //=======================回首頁按鈕===========================
+        var msg = new builder.Message(session);
+        msg.suggestedActions(builder.SuggestedActions.create(
+            session, [
+                builder.CardAction.imBack(session, "回首頁", "回首頁")
+            ]
+        ));
+        session.send(msg);
+        // ==========================================================
+    },
+    function (session, results) {
+        var id = results.response;
         var options = {
             method: "GET",
-            url: "https://www.quandl.com/api/v3/datasets/CME/CLZ2018.json",
-//             qs: { function: "TIME_SERIES_DAILY", symbol: id, apikey: "BSVAQV1T4LLSEVKZ" 
-//         },
-            json:ture
+            url: "https://www.alphavantage.co/query",
+            //寫在api url ?後面的參數，要放在qs(key)的Json set內
+            qs: {
+                function: "TIME_SERIES_DAILY",
+                symbol: id,
+                apikey: "2C8MUXABNVMED4DS"
+            },
+            //指定json格式的輸出
+            json: true
         }
-            request(options, function (error, response, body) {
-                var oil = body;
-                //建立日期物件，放入今天的日期
-                var d = new Date();
-                //當日期是周末，則將日期回到上個周五
-                if (d.getDay()==0)
-                d.setDate(d.getDate()-1);
-                if (d.getDay()==1)
-                d.setDate(d.getDate()-2);
-                
-                // 將日期改成ISO規則日期的第0-10個字元 YYYY-mm-dd
-                
-                //TODO: 更好的方式是用RegExpression, 找出JSON檔第一筆日期的資料，可以避免節慶日找不到資料
-            
-                var tradeday = d.toISOString().slice(0, 10);
-                var getoil = oil["dataset"]["data"][0][4]
-                session.endDialog(`${tradeday} close at : $${getoil}`);
-         
-    });
-    });
+        request(options, function (error, response, body) {
+            var stock = body;
+            if (stock["Time Series (Daily)"]) {
+                //用RegExpression, 找出JSON檔第一筆日期的資料，可以避免節慶日找不到資料
+                var date = JSON.stringify(stock["Time Series (Daily)"]).match(/\d{4}-\d{2}-\d{2}/);
+                //parseFloat 將文字改成Float type, toFixed(2)將數字縮到小數點2位數
+                var open = parseFloat(stock["Time Series (Daily)"][date]["1. open"]).toFixed(2)
+                var high = parseFloat(stock["Time Series (Daily)"][date]["2. high"]).toFixed(2)
+                var low = parseFloat(stock["Time Series (Daily)"][date]["3. low"]).toFixed(2)
+                var close = parseFloat(stock["Time Series (Daily)"][date]["4. close"]).toFixed(2)
+                session.send(`${id.toUpperCase()} : ${date} \nopen $${open}\nhigh $${high}\nlow $${low}\nclose $${close}`);
+                session.replaceDialog('us');
+            } else {
+                session.send(`沒有找到這個股票!`);
+                session.replaceDialog('us');
+            }
+        });
+    }
+])
+
+
+bot.dialog('euron', [
+    function (session) {
+        builder.Prompts.text(session, "請輸入歐股Ticker:");
+
+        //=======================回首頁按鈕===========================
+        var msg = new builder.Message(session);
+        msg.suggestedActions(builder.SuggestedActions.create(
+            session, [
+                builder.CardAction.imBack(session, "回首頁", "回首頁")
+            ]
+        ));
+        session.send(msg);
+        // ==========================================================
+    },
+    function (session, results) {
+        var id = results.response;
+        var euron = "https://www.quandl.com/api/v3/datasets/EURONEXT/" + id.toUpperCase() + ".json"
+        var options = {
+            method: "GET",
+            url: euron,
+            //寫在api url ?後面的參數，要放在qs(key)的Json set內
+            qs: {
+                 apikey: "Cusz1VPxbAaU8Q72Y2i4"
+                 },
+            //指定json格式的輸出
+            json: true
+        }
+        request(options, function (error, response, body) {
+            var stock = body;
+            if(stock["dataset"]){
+                //用RegExpression, 找出JSON檔第一筆日期的資料，可以避免節慶日找不到資料
+                // var date = JSON.stringify(stock["dataset"]).match(/\d{4}-\d{2}-\d{2}/);
+                //parseFloat 將文字改成Float type, toFixed(2)將數字縮到小數點2位數
+                var open = parseFloat(stock["dataset"]["data"][0][1]).toFixed(2)
+                var high = parseFloat(stock["dataset"]["data"][0][2]).toFixed(2)
+                var low = parseFloat(stock["dataset"]["data"][0][3]).toFixed(2)
+                var last = parseFloat(stock["dataset"]["data"][0][4]).toFixed(2)
+                session.endDialog(`${id.toUpperCase()} : \nopen $${open}\nhigh $${high}\nlow $${low}\nclose $${last}`);
+                session.replaceDialog('euron');
+            }else {
+                session.send(`沒有找到這個股票!`);
+                session.replaceDialog('euron');
+            }
+        });
+    }
+])
