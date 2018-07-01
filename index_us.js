e7d0d6af740d37cdc1ded8ee26b87a2e677c5b46
chiehchih1110 <chiehchih1110@hotmail.com>
2018-07-01 14:47:15 +0800

    commit chiehchih
    index_metal  favorite

--- index_us.js
+++ index_us.js
@@ -1,33 +1,26 @@
-//import module
+// #region 基本內容 勿改!!
 var restify = require("restify");
 var builder = require("botbuilder");
 var request = require("request");
-
-//Setup Web Server
 var server = restify.createServer();
-
 server.listen(process.env.port || process.env.PORT || "3978", function () {
     console.log('%s listening to %s', server.name, server.url);
 });
-
 //create chat connector for communicating with the bot framework service
-
 var connector = new builder.ChatConnector({
     appId: process.env.MicrosoftAppId,
     appPassword: process.env.MicrosoftAppPassword,
 });
-
 //Listen for messages from users
 server.post('/api/messages', connector.listen());
-
-// create your bot with a function to receive messages from user
 // 歡迎訊息頁
 var bot = new builder.UniversalBot(connector,
     function (session) {
-        session.send('歡迎來到FinTastic');
+        session.send('=== 歡 迎 來 到 F i n T a s t i c ===');
         session.replaceDialog('mainMenu')
     });
-
+// #endregion
+// #region 首頁 - 需要修改
 bot.dialog('mainMenu', [
     function (session) {
         builder.Prompts.choice(session, "請問要查詢什麼?", ["美股", "匯率", "台股", "港股", "日股", "黃金"], { listStyle: builder.ListStyle.button });
@@ -40,13 +33,52 @@ bot.dialog('mainMenu', [
             session.replaceDialog('gold');
         // TODO 加入每個人寫的功能
     }
-]).triggerAction({ matches: /^回首頁$/ }); //使用者任何時間打入"回首頁"都可以回到首頁
+]).triggerAction({ matches: /^回首頁$/ }); //任何時間打入"回首頁"都可以回到此Dialog
+// #endregion 首頁
+// #region 共用的sheetDB function 勿改!!===============
+//=========== function 新增Ticker sheetDB =================
+function addToSheetDB(ticker, column, returnDialog, session) {
+    // 設定要加入到SheetDB的欄位名(colume), 與儲存內容(ticker)
+    var body_data = `[{"${column}" : "${ticker}"}]`;
+    request({
+        uri: 'https://sheetdb.io/api/v1/5b35ec114e823',
+        json: true,
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: {"data": body_data}
+    }, function (error, response, body) {
+        if (!error && response.statusCode == 201) {
+            session.send(ticker+"儲存成功");
+            session.replaceDialog(returnDialog);
+        } else {
+            console.log(error)
+        }
+    });
+}
 
+//=========== function 刪除Ticker sheetDB =================
+function deleteToSheetDB(ticker, column, returnDialog, session) {
+    request({
+        // 設定要加入到SheetDB的欄位名(colume), 與儲存內容(ticker)
+        uri: 'https://sheetdb.io/api/v1/5b35ec114e823/'+column+'/' + ticker,
+        json: true,
+        method: "DELETE",
+        headers: { "Content-Type": "application/json" },
+    }, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            session.send(ticker+"刪除成功");
+            session.replaceDialog(returnDialog);
+        } else {
+            console.log(error)
+        }
+    });
+}
+// #endregion =====sheetDB====================================================
+// #region ===================== (us) 美 股 首 頁 ==============================
 bot.dialog('us', [
     function (session) {
         builder.Prompts.text(session, "請輸入美股Ticker:");
-
-        //=======================回首頁按鈕===========================
+        //=======================推 薦 按 鈕===========================
         var msg = new builder.Message(session);
         msg.suggestedActions(builder.SuggestedActions.create(
             session, [
@@ -93,13 +125,13 @@ bot.dialog('us', [
     }
 ])
 
-//=================== 列 印 我 的 最 愛 ===================
+//===================(us) 列 印 我 的 最 愛 ===================
 bot.dialog('us_favorite', [
     function (session) {
+        //設定要查詢sheetDB的資料
         var options = {
             method: "GET",
             url: "https://sheetdb.io/api/v1/5b35ec114e823",
-            //指定json格式的輸出
             json: true
         };
         request(options, function (error, response, body) {
@@ -116,9 +148,8 @@ bot.dialog('us_favorite', [
 ]).triggerAction({ matches: /^我的最愛$/ });
 
 
-//============== 印 出 我 的 最 愛 ==================
-async function showPrice(usticker, session) {
-    console.log("beforeRequest: " + usticker);
+//============== 印 出 我 的 最 愛 的 Function ==================
+function showPrice(usticker, session) {
     var options = {
         method: "GET",
         url: "https://www.alphavantage.co/query",
@@ -135,124 +166,64 @@ async function showPrice(usticker, session) {
             var date = JSON.stringify(stock["Time Series (Daily)"]).match(/\d{4}-\d{2}-\d{2}/);
             var close = parseFloat(stock["Time Series (Daily)"][date]["4. close"]).toFixed(2);
             var msg = usticker.toUpperCase() + " " + date + " close $" + close;       
-            console.log("after request: " + msg);
-            // session.send(msg);
+            // 每次request資料近來，就加到變數 session.dialogData.msg
             session.dialogData.msg += msg+"\n";
-            session.dialogData.count += 1;
-            //TODO 當msg已經與 session.dialogData.fav.length 相同，則執行下面兩個
+            // 每次request資料近來，就紀錄(已完成的次數+1)
+            session.dialogData.count += 1;  
+            // 當(已完成)次數與session.dialogData.fav.length(我的最愛名單的長度)相同，則執行 1列印 2回到美股首頁
             if (session.dialogData.count == session.dialogData.fav.length) {
-            session.send(session.dialogData.msg)
-            session.replaceDialog('us');
+                session.send(session.dialogData.msg)
+                session.replaceDialog('us');
             }
-            // return msg
         } else {
-            session.send(`沒有找到這個股票!`);
+            session.send(`沒有找到${usticker}`);
         }
     });
 }
 
-//=========== 新 增 股 票 到 我 的 最 愛 =============
-bot.dialog('us_add_favorite', [
+
+//============= 新 增 股 票 到 我 的 最 愛 ===============
+bot.dialog('add_favorite', [
     function (session) {
-        builder.Prompts.text(session, "請輸入要新增的美股: ");
+        builder.Prompts.text(session, "請輸入要新增的美股:");
     },
     function (session, results) {
-        session.dialogData.addus = results.response;
-        addToSheetDB(session.dialogData.addus, session);
-
+        session.dialogData.addTicker = results.response;
+        //呼叫addToSheetDB function, 將收到的Ticker存入sheetDB, 
+        //column = 存入的google試算表欄位名稱; returnDialog = 完成後回到哪個dialog
+        addToSheetDB(session.dialogData.addTicker.toUpperCase(), column="usticker", returnDialog="us", session);
     }
 ]).triggerAction({ matches: /^新增最愛$/ });
 
 
-//========== === 刪 除 我 的 最 愛 股 票 =============
-bot.dialog('us_del_favorite', [
+//================ 刪 除 我 的 最 愛 股 票 =================
+bot.dialog('del_favorite', [
     function (session) {
-        builder.Prompts.text(session, "請輸入要刪除的美股: ");
+        builder.Prompts.text(session, "請輸入要刪除的美股:");
     },
     function (session, results) {
-        session.dialogData.deleteus = results.response;
-        deleteToSheetDB(session.dialogData.deleteus, session);
-    }
-]).triggerAction({ matches: /^刪除最愛$/ });
-
-//=========== function 新增 sheetDB =================
-function addToSheetDB(usticker, session) {
-    console.log("addToSheetDB" + usticker);
-    request({
-        uri: 'https://sheetdb.io/api/v1/5b35ec114e823',
-        json: true,
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: {
-            "data": [{
-                "usticker": usticker
-            }]
-        }
-    }, function (error, response, body) {
-        if (!error && response.statusCode == 201) {
-            session.send("儲存成功");
-            session.replaceDialog('us');
-        } else {
-            console.log(error)
-        }
-    });
-}
-
-//=========== function 刪除 sheetDB =================
-function deleteToSheetDB(usticker, session) {
-    console.log("addToSheetDB" + usticker);
-    request({
-        uri: 'https://sheetdb.io/api/v1/5b35ec114e823/usticker/' + usticker,
-        json: true,
-        method: "DELETE",
-        headers: { "Content-Type": "application/json" },
-    }, function (error, response, body) {
-        if (!error && response.statusCode == 200) {
-            session.send("刪除成功");
-            session.replaceDialog('us');
-        } else {
-            console.log(error)
-        }
-    });
-}
-//===================================================
-
-bot.dialog('gold', [
-    function (session) {
-        builder.Prompts.text(session, "請輸入gold:");
-    },
-    function (session, results) {
-
+        session.dialogData.delTicker = results.response;
+        //先查詢Ticker是否存在sheetDB
+        //設定要查詢sheetDB的資料
         var options = {
             method: "GET",
-            url: "https://www.quandl.com/api/v3/datasets/CME/GCZ2018.json?",
-            // 寫在api url ?後面的參數，要放在qs(key)的Json set內
-            // qs:{
-            //     api_key="sae2Txxu_kQTHFHDxyjr"
-            // }, 
-            // 指定json格式的輸出
+            url: "https://sheetdb.io/api/v1/5b35ec114e823",
             json: true
-        }
+        };
         request(options, function (error, response, body) {
-            var gold = body;
-            // 建立日期物件，放入今天的日期
-            var d = new Date();
-            // 當日期是周末，則將日期回到上個周五
-            if (d.getDay() == 0)
-                d.setDate(d.getDate() - 1);
-            if (d.getDay() == 1)
-                d.setDate(d.getDate() - 2);
-            // 將日期改成ISO規則日期的第0-10個字元 YYYY-mm-dd
+            session.dialogData.myFav = body;
+            //TODO 檢查要刪除的Ticker 是否在sheetDB內, 如果有就刪除Ticker, 沒有就回錯誤訊息
+            //console.log("==========="+session.dialogData.myFav.usticker+"============")
+            // 1 將session.dialogData.myFav 變成一個字串
+            // 2 使用regex比對Ticker是否有在裡面
+            //=========================================================================
 
-            // TODO:更好的方式是用RegExpression,找出JSON檔第一筆日期的資料,可以避免節慶日找不到資料
-
-            var tradeday = d.toISOString().slice(0, 10);
-            var getgold = gold["dataset"]["data"][0][4]
-            session.endDialog(`${tradeday} close at : $${getgold}`);
-        });
-        // TODO 讓request資料已經完成後，才執行session.replaceDialog
-        session.endConversation();
-        session.replaceDialog('gold');
+            if (!error && response.statusCode == 200) {
+            //呼叫deleteToSheetDB function, 將收到的Ticker從sheetDB刪除
+            //column = 刪除的google試算表欄位名稱; returnDialog = 完成後回到哪個dialog
+            deleteToSheetDB(session.dialogData.delTicker.toUpperCase(), column="usticker", returnDialog="us", session);       
+            }
+        });        
     }
-]);
-// TODO 提供一個trigger event, 讓使用者可以回到首頁選單
+]).triggerAction({ matches: /^刪除最愛$/ });
+// #endregion ======美股結束=============================
